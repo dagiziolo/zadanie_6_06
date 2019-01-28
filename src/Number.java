bd746a734a73a8247daa7093463a1711d0c0a158
dagma <dagmara.ziolkowska@wp.pl>
2019-01-28 12:52:27 +0100

    Uproszczenie pętli usunięcie break i warunku 1==1

--- src/Number.java
+++ src/Number.java
@@ -5,20 +5,37 @@ public class Number {
         Scanner scan = new Scanner(System.in);
         int x;
 
-        do {
-            System.out.println("Podaj liczbę");
-            x = scan.nextInt();
+//        do {
+//            System.out.println("Podaj liczbę");
+//            x = scan.nextInt();
+//            if (x < 100) {
+//                System.out.println("Podana liczba jest za mała");
+//            } else if (x > 200) {
+//                System.out.println("Podana liczba jest za duża");
+//            } else if (x % 3 != 0) {
+//                System.out.println("Podana liczba nie jest podzielna przez 3");
+//            } else {
+//                System.out.println("Twoja liczba jest ok");
+//                break;
+//            }
+//        } while (1==1); 
+
+        System.out.println("Podaj liczbę");
+        x = scan.nextInt();
+        while (x < 100 || x > 200 || x % 3 != 0) {
+
             if (x < 100) {
                 System.out.println("Podana liczba jest za mała");
             } else if (x > 200) {
                 System.out.println("Podana liczba jest za duża");
-            } else if (x % 3 != 0) {
-                System.out.println("Podana liczba nie jest podzielna przez 3");
             } else {
-                System.out.println("Twoja liczba jest ok");
-                break;
+                System.out.println("Podana liczba nie jest podzielna przez 3");
             }
-        } while (1==1);
-
+            System.out.println("Podaj liczbę");
+            x = scan.nextInt();
+        }
+        System.out.println("Twoja liczba jest ok");
     }
+
 }
+
